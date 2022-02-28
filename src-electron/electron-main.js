da4602db6ee3d6ed47328453c21f73abe9d0ca70
Mykola Zhyhallo <mzhyhall@gmail.com>
2022-02-28 02:07:52 +0100

    Workers slider

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -49,7 +49,7 @@ function createWindow () {
     mainWindow = null
   })
 
-  const doser = new Doser(true, 32)
+  const doser = new Doser(true, 16)
   const window = mainWindow
   doser.listen('atack', (data) => console.log(data.log))
   doser.listen('atack', (data) => window.webContents.send('atack', data))
