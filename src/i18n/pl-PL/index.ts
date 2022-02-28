03b577df8d222200d28a2786e30f88faeb4f8710
adam <adam@razniewski.eu>
2022-02-28 02:22:52 +0100

    Added more translations

--- src/i18n/pl-PL/index.ts
+++ src/i18n/pl-PL/index.ts
@@ -1,6 +1,8 @@
 export default {
+  language: 'Język',
   ddos: {
     description: 'Ataki DDOS robią bardzo dużo żądań do serwera wroga. Serwery dzięki temu zaczynają lagować i przestają przyjmować nowe żądania.',
+    coordinators: 'Koordynacja https://t.me/incourse911 ',
     counter: {
       atackedTimes: 'Zaatakowano już:',
       currentTarget: 'Aktualny cel: '
@@ -12,6 +14,14 @@ export default {
     proxy: {
       name: 'Atakuj tylko z PROXY',
       description: 'Bez tej opcji program będzie używał Twojego IP. Jest to o wiele bardziej wydajne, Twoje ataki są mocniejsze, ale wróg poznaje Twój adres IP i nie będziesz anonimowy.'
+    },
+    advanced: {
+      header: 'Zaawansowane ustawienia',
+      description: 'Jeżeli nie jesteś doświadczonym użytkownikiem nie zmieniaj ustawień zaawansowanych. Może to mieć zły wpływ na działanie programu',
+      masDosersCount: {
+        name: 'Рівень паралельності (TODO)',
+        description: 'Задає кількість паралельних процесів. Використовуються асинхронічні механізми (не потокові), тому це не сильно впливає на CPU/RAM. Наразі опція діє в ручному режимі і задає кількість паралельних запитів. При великій кількості запитів, проксі сервера можуть блокувати DDOS. Змінюйте вартість і чекайте 2-3 хвилини, щоб побачити результат. Якщо бачите ECONNABORTED - це значить Вас блокують і треба зменшити кількість.'
+      }
     }
   }
 }
--- src/i18n/ua-ua/index.ts
+++ src/i18n/ua-ua/index.ts
@@ -1,6 +1,3 @@
-// This is just an example,
-// so you can safely delete all default props below
-
 export default {
   language: 'Мова',
   ddos: {
