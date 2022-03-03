484837ab26cc8337173c4cf6478b25de51b8355f
v-baida <vladyslav.d.baida@gmail.com>
2022-03-03 12:45:59 +0200

    Changed docker installation process, made a script folder

--- scripts/azure-custom-data-script.sh
+++ scripts/azure-custom-data-script.sh
@@ -4,24 +4,16 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    wget
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+wget -O - https://get.docker.com/ | bash
 
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-
 chmod +x ~/.docker/cli-plugins/docker-compose
 sudo chown $USER /var/run/docker.sock
 
