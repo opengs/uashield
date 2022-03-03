98b7bfa22aade6ccb7b8ad425520026cbfaeb4e7
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2022-03-03 17:41:13 +0100

    i18n: es

--- src/i18n/es-ES/index.ts
+++ src/i18n/es-ES/index.ts
@@ -2,6 +2,7 @@ export default {
   language: 'Idioma',
   ddos: {
     description: 'Ataque DDOS que realiza muchas requests/solicitudes a targets de servidores enemigos. Los targets/objetivos comenzarán a funcionar mal o con lag.',
+    coordinators: 'Coordinación de acciones: https://t.me/incourse911 ',
     counter: {
       attackedTimes: 'Número de ataques:',
       currentTarget: 'Target/Objetivo actual:'
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
+import esES from './es-ES'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
+  'es-ES': esES,
   'pl-PL': plPL
 }
