280c0bcea24f66bab04ca110a559550b7a2ad171
nedjulius <julius.nedzinskas@gmail.com>
2022-03-04 17:11:17 -0500

    Add Lithuanian translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
+import ltLT from './lt-LT'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
-  'pl-PL': plPL
+  'pl-PL': plPL,
+  'lt-LT': ltLT
 }
