749d08448bd2c5dab6124586a6d80550dddbcf2e
Ilya.B <ilya.buligin.w@gmail.com>
2022-03-05 00:40:02 +0100

    improve es translation

--- src/i18n/es-ES/index.ts
+++ src/i18n/es-ES/index.ts
@@ -2,6 +2,7 @@ export default {
   language: 'Idioma',
   ddos: {
     description: 'Ataque DDOS que realiza muchas requests/solicitudes a targets de servidores enemigos. Los targets/objetivos comenzarán a funcionar mal o con lag. https://help-ukraine-win.com/',
+    coordinators: 'Para fines de coordinación: https://discord.gg/7BfJ9JKQ98',
     counter: {
       attackedTimes: 'Número de ataques:',
       currentTarget: 'Target/Objetivo actual:'
@@ -13,6 +14,19 @@ export default {
     proxy: {
       name: 'Atacar sólo usando proxy',
       description: 'Sin esta opción, el programa permitirá usar tu IP. Realizarás ataques más eficientes, pero los ataques no serán anónimos.'
+    },
+    advanced: {
+      header: 'Ajustes avanzados',
+      description: 'Si alguna terminología no le resulta clara, no cambie la configuración ampliada. Puede provocar un efecto negativo y disminuir la eficacia de la aplicación.',
+      masDosersCount: {
+        name: 'El nivel de paralelismo',
+        description: 'Determina el número de procesos paralelos. La aplicación se basa en operaciones asíncronas (no en flujos paralelos), por lo que la CPU/RAM no se sobrecarga. Después de la configuración, los cambios suelen tardar unos 10 segundos en surtir efecto.'
+      }
+    },
+    update: {
+      title: 'Actualización disponible',
+      cancel: 'Cancelar',
+      confirm: 'Instalar actualizaciones'
     }
   }
 }
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
+import esEs from './es-ES'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
-  'pl-PL': plPL
+  'pl-PL': plPL,
+  'es-ES': esEs
 }
