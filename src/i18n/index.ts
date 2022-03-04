406d2ec216a27997808166137dd738d9bad8efa3
nedjulius <julius.nedzinskas@gmail.com>
2022-03-04 17:46:50 -0500

    Add new translations to dropdown

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,10 +2,18 @@ import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
 import ltLT from './lt-LT'
+import csCS from './cs-CS'
+import deDE from './de-DE'
+import nlNL from './nl-NL'
+import skSK from './sk-SK'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
   'pl-PL': plPL,
-  'lt-LT': ltLT
+  'lt-LT': ltLT,
+  'cs-CS': csCS,
+  'de-DE': deDE,
+  'nl-NL': nlNL,
+  'sk-SK': skSK
 }
