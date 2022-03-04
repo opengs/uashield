915e208a2073d26cd37584de48ba8136e2f78c36
mmtka <48442554+mmtka@users.noreply.github.com>
2022-03-04 23:15:53 +0100

    Update index.ts
    eport default with new translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,15 @@
 import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
+import skSK from './sk-SK'
+import czCZ from './cz-CZ'
+import deDE from './de-DE'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
-  'pl-PL': plPL
+  'pl-PL': plPL,
+  'sk-SK': skSK,
+  'cz-CZ': czCZ,
+  'de-DE': deDE
 }
