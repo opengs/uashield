822f191918ea7fee0e60fb663aac4bea12c2e7a9
Julius <45975811+nedjulius@users.noreply.github.com>
2022-03-05 13:38:57 -0500

    Added new translations (LT, CS, DE, NL, SK) (#107)
    * Add Lithuanian translations
    
    * Add Czech translations
    
    * Add German translations
    
    * Add Dutch translations
    
    * Add Slovak translations
    
    * Add new translations to dropdown
    
    * Add additional space to currentTarget
    
    Co-authored-by: Adam Raźniewski <adam@razniewski.eu>

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,21 @@
 import enUS from './en-US'
 import uaUa from './ua-ua'
 import plPL from './pl-PL'
+import ltLT from './lt-LT'
+import csCS from './cs-CS'
+import deDE from './de-DE'
+import nlNL from './nl-NL'
+import skSK from './sk-SK'
 import esES from './es-ES'
 
 export default {
   'en-US': enUS,
   'ua-UA': uaUa,
   'pl-PL': plPL,
+  'lt-LT': ltLT,
+  'cs-CS': csCS,
+  'de-DE': deDE,
+  'nl-NL': nlNL,
+  'sk-SK': skSK,
   'es-ES': esES
 }
---
+++ src/i18n/sk-SK/index.ts
@@ -0,0 +1,32 @@
+export default {
+  language: 'Jazyk',
+  ddos: {
+    description: 'Útok DDoS generuje množstvo požiadavok na cieľový nepriateľský server. Cieľový server začne meškať a prestane správne fungovať.',
+    coordinators: 'Koordinátori https://discord.gg/7BfJ9JKQ98 ',
+    counter: {
+      attackedTimes: 'Časy útoku:',
+      currentTarget: 'Aktuálny cieľ: '
+    },
+    enable: {
+      name: 'DDoS útok',
+      description: 'Povoliť DDoS útok na kritické nepriateľské servery: vládu, bankové systémy, propagandistické webové stránky atď'
+    },
+    proxy: {
+      name: 'Útočte iba pomocou proxy',
+      description: 'Bez tejto možnosti bude program môcť používať vašu IP. Tak urobí efektívnejší útok , ale vaše útoky nebudú anonymné.'
+    },
+    update: {
+      title: 'Aktualizácia k dispozícii',
+      cancel: 'Zrušiť',
+      confirm: 'Nainštalujte aktualizácie'
+    },
+    advanced: {
+      header: 'Rozšírené nastavenia',
+      description: 'Ak vám niektorá terminológia nie je jasná, nemeňte rozšírené nastavenia. Môže to viesť k negatívnemu účinku a zníženiu účinnosti aplikácie',
+      masDosersCount: {
+        name: 'Úroveň paralelizmu',
+        description: 'Určuje počet paralelných procesov. Program sa spolieha na asynchrónne operácie (nie paralelné medzi vláknami), takže nedochádza k preťaženiu CPU / RAM. Po nastavení zvyčajne trvá asi 10 sekúnd, kým sa počet paralelných procesov prispôsobí k novým nastaveniam.'
+      }
+    }
+  }
+}
