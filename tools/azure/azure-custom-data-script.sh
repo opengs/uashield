ce93be1c4edeb4170f9fccf833b73b01898fff90
adam <adam@razniewski.eu>
2022-03-06 19:20:20 +0100

    Changed script to down and timeout only for one second, added restart always

--- tools/azure/azure-custom-data-script.sh
+++ tools/azure/azure-custom-data-script.sh
@@ -22,6 +22,7 @@ version: \"3.3\"
 services:
   worker:
     image: ghcr.io/opengs/uashield:latest
+    restart: always
     command:
       - \"7500\"
       - \"true\"" >> /home/docker-compose.yaml
@@ -32,6 +33,8 @@ cd /home/
 
 sudo docker-compose pull && sudo docker-compose up -d --scale worker=$(grep -c ^processor /proc/cpuinfo)
 
-sudo echo "*/30 * * * * cd /home/ && sudo docker-compose down && sudo docker-compose pull && sudo docker-compose up -d --scale worker=$(grep -c ^processor /proc/cpuinfo)" >> /home/cronjob
-sudo echo "@reboot cd /home/ && sudo docker-compose down && sudo docker-compose pull && sudo docker-compose up -d --scale worker=$(grep -c ^processor /proc/cpuinfo)" >> /home/cronjob
+sudo echo "*/30 * * * * cd /home/ && sudo docker-compose down -t 1 && sudo docker-compose pull && sudo docker-compose up -d --scale worker=$(grep -c ^processor /proc/cpuinfo)" >> /home/cronjob
+
+# restart:always should do the job to run container on startup, but the hard restart is good here to avoid problems
+sudo echo "@reboot cd /home/ && sudo docker-compose down -t 1 && sudo docker-compose pull && sudo docker-compose up -d --scale worker=$(grep -c ^processor /proc/cpuinfo)" >> /home/cronjob
 crontab /home/cronjob
