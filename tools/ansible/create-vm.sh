255bbc20ccb0b7876e06c2ddc2e141437329187d
Denys Butenko <ua13dark@gmail.com>
2022-03-16 15:09:54 +0530

    Add uasword playbook

--- tools/ansible/create-vm.sh
+++ tools/ansible/create-vm.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
-# Usage example
-# ./create-vm.sh eastus2
 
-# When you get started you need to create Resource Group for VMs
-# az group create --name 'STOPWAR' --location 'uaenorth'
+# Prerequisites
+# Have installed azure cli on your machine
+# Have been logged in to your account. Run `az login`
+
+# Usage example
+# ./create-vm.sh
+# ./create-vm.sh ua-loadtest
+# ./create-vm.sh uasword
 
 # Get location list grouped by RegionName
 # az account list-locations --query "[?not_null(metadata.latitude)] .{RegionName:name}" --output json
 
 # Variables
 INPUT=$1
-LOCATION="${INPUT:-norwayeast}"
-RESOURCE_GROUP='STOPWAR'
+PROJECT="${INPUT:-uashield}"
+AVAILABLE_LOCATIONS='southindia; eastasia; centralindia; koreacentral; australiacentral; uaenorth;'
+LOCATIONS=$(echo $AVAILABLE_LOCATIONS | tr ";" "\n")
 USER='azureuser'
 IMAGE='UbuntuLTS'
 
-for i in {1..4}
-do
-  NAME=uashield-$LOCATION-unit-$i
-  echo "$NAME : Initializing vm..."
-  az vm create --admin-user $USER --location $LOCATION --name $NAME --resource-group $RESOURCE_GROUP --image $IMAGE --generate-ssh-keys
+
+for nextLocation in $LOCATIONS
+  do
+    GROUP_NAME=$PROJECT-$nextLocation-group
+    az group create --name $GROUP_NAME  --location $nextLocation
+
+    for i in {1..4}
+    do
+      NAME=$PROJECT-$nextLocation-unit-$i
+      echo "$NAME : Initializing vm..."
+      az vm create --admin-user $USER --location $nextLocation --name $NAME --resource-group $GROUP_NAME --image $IMAGE --generate-ssh-keys --public-ip-sku Standard
+    done
 done
 
 bash ./update-hosts.sh
