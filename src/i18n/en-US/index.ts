0fae2965bff9faccea217487e843fde32c3eca5b
andrsim66 <sevenander@gmail.com>
2022-03-21 10:02:17 +0100

    added Korean; adjusted UA, EN, PL translations;

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -13,7 +13,7 @@ export default {
     },
     proxy: {
       name: 'Attack only using proxy',
-      description: 'Without this option, program will be allowed to use your IP. You will make more efficient attack, but attacks will not be anonymous.'
+      description: 'Without this option, the program will use the actual IP address of your computer. This allows to make the attack much more efficient, but attacks will not be anonymous and your IP address will be visible to the enemy.'
     },
     update: {
       title: 'Update available',
@@ -22,10 +22,10 @@ export default {
     },
     advanced: {
       header: 'Extended settings',
-      description: 'If some terminology is not clear for you, please do not change extended settings. It can lead to negative effect and decrease effectiveness of the application',
+      description: 'If some terminology is not clear for you, please do not change extended settings. It can lead to negative effects and decrease the effectiveness of the application.',
       masDosersCount: {
         name: 'Level of parallelism',
-        description: 'Defines the amount of dosers. Application relies on asynchronous operations (not parallel via threads), thanks to that CPU/RAM are not overloaded. Once adjusted, it will usually take around 10 seconds to adjust amount of the dosers according to new settings.'
+        description: 'Defines the number of attack processes. The application relies on asynchronous operations (not parallel via threads), thanks to that CPU/RAM are not overloaded. Once adjusted, it will usually take around 10 seconds to adjust the amount of the attack processes according to new settings.'
       }
     }
   }
--- src/i18n/pl-PL/index.ts
+++ src/i18n/pl-PL/index.ts
@@ -1,7 +1,7 @@
 export default {
   language: 'Język',
   ddos: {
-    description: 'Ataki DDoS robią bardzo dużo żądań do serwera wroga. Serwery dzięki temu zaczynają lagować i przestają przyjmować nowe żądania. Szczegóły tutaj: https://help-ukraine-win.com/',
+    description: 'Ataki DDoS robią bardzo dużo żądań do serwera wroga. Serwery dzięki temu zaczynają lagować i przestają przyjmować nowe żądania. Szczegóły tutaj: https://help-ukraine-win.com.ua/',
     coordinators: 'Koordynacja https://discord.gg/7BfJ9JKQ98 ',
     counter: {
       attackedTimes: 'Zaatakowano już:',
@@ -13,14 +13,14 @@ export default {
     },
     proxy: {
       name: 'Atakuj tylko z PROXY',
-      description: 'Bez tej opcji program będzie używał Twojego IP. Jest to o wiele bardziej wydajne, Twoje ataki są mocniejsze, ale wróg poznaje Twój adres IP i nie będziesz anonimowy'
+      description: 'Bez tej opcji program będzie używał Twojego IP. Jest to o wiele bardziej wydajne. Twoje ataki są mocniejsze, ale wróg pozna ten adres IP i przestaniesz być anonimowy.'
     },
     advanced: {
       header: 'Zaawansowane ustawienia',
-      description: 'Jeżeli nie jesteś doświadczonym użytkownikiem nie zmieniaj ustawień zaawansowanych. Może to mieć zły wpływ na działanie programu',
+      description: 'Jeżeli nie jesteś doświadczonym użytkownikiem nie zmieniaj ustawień zaawansowanych. Może to mieć zły wpływ na działanie programu.',
       masDosersCount: {
         name: 'Poziom równoległości',
-        description: 'Określa liczbę równoległych procesów. Stosowane są mechanizmy asynchroniczne (nie strumieniowe), więc nie ma to dużego wpływu na procesor / RAM. Obecnie opcja działa w trybie manualnym i określa liczbę równoległych zapytań. Przy dużej liczbie requestów proxy serwery mogą blokować DDOS. Zmień znaczenie i poczekaj 2-3 minuty, aby zobaczyć wynik. Jeśli widzisz ECONNABORTED - oznacza to, że jesteś zablokowany i musisz zmniejszyć liczbę/ilość'
+        description: 'Definiuje ilość ataków uruchomionych jednocześnie , Aplikacja korzysta z operacji asynchronicznych a nie równoległych poprzez wątki, dzięki czemu CPU/RAM nie są mocno obciążone. Po zmianie wartości proces jej wprowadzania trwa około 10 sekund.'
       }
     },
     update: {
