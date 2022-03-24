014efc66e7a0849286412d170dea33deab118ca4
RuiMansilha <ruimansilha@gmail.com>
2022-03-24 11:10:03 +0000

    2 files modified

--- src/i18n/pt-PT/index.ts
+++ src/i18n/pt-PT/index.ts
@@ -1,12 +1,13 @@
 export default {
   language: 'Idioma',
   ddos: {
-    description: 'Ataques de DDoS são um método de impedir o funcionamento de páginas web após o envio de diversas solicitações que excedem a capacidade da rede. O servidor de destino ficará lento e a parar de funcionar corretamente',
-    coordinators: 'Coordenação de ações https://discord.gg/7BfJ9JKQ98 ',
+    description: 'Ataques de DDoS são métodos que tornam os recursos de um sistema indisponíveis para os seus utilizadores através de diversas solicitações que excedem a capacidade da rede',
+    coordinators: 'Coordenação de ações: https://discord.gg/7BfJ9JKQ98 ',
     counter: {
       attackedTimes: 'Número de ataques:',
       currentTarget: 'Objetivo atual: '
     },
+    efficiency: 'Eficiência',
     enable: {
       name: 'Ataque DDoS',
       description: 'Habilitar o ataque DDoS em servidores inimigos críticos: governo, sistemas bancários, páginas de propaganda, etc'
@@ -23,9 +24,13 @@ export default {
     advanced: {
       header: 'Configurações avançadas',
       description: 'Se alguma terminologia não estiver clara, não altere as configurações avançadas. Pode levar a um efeito negativo e diminuir a eficácia da aplicação',
+      automaticMode: {
+        name: 'Modo automático',
+        description: 'Quando habilitado, o programa analisa a eficácia e seleciona automaticamente o nível de processos paralelos.'
+      },
       masDosersCount: {
         name: 'Nível de processos paralelos',
-        description: 'Define a quantidade de processos paralelos. A aplicação depende de operações assíncronas (não paralelas via threads), graças a isto a CPU/RAM não são sobrecarregadas. Uma vez ajustado, geralmente leva cerca de 10 segundos para ajustar a quantidade processos paralelos de acordo com as novas configurações.'
+        description: 'Define a quantidade de processos paralelos. São mecanismos assíncronos para não sobrecarregar a CPU / RAM. Uma vez ajustado, leva cerca de 10 segundos para assumir as novas configurações.'
       }
     }
   }
