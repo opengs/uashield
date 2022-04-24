18301f3750c0f11c5a63e68d6ba0eeb3a29dbc8c
Mykola Zhyhallo <mzhyhall@gmail.com>
2022-04-24 09:58:29 +0200

    Better machine ID

--- src-electron/analytics.js
+++ src-electron/analytics.js
@@ -1,7 +1,17 @@
 import ua from 'universal-analytics'
-import uuid4 from 'uuid4'
 
-export const usr = ua('UA-222593827-1', uuid4())
+import uuid4 from 'uuid4'
+import {machineIdSync} from 'node-machine-id';
+
+function makeId() {
+  try {
+    return machineIdSync()
+  } catch {
+    return uuid4()
+  }
+}
+
+export const usr = ua('UA-222593827-1', makeId())
 
 export function trackEvent (category, action, label, value) {
   const r = usr.event({ ec: category, ea: action, el: label, ev: value }).send()
